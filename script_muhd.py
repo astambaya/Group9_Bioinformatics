218e293d9b2e42218fc4dfc4e31bbdcfcb1a142e
Muhammadyusufsaid3 <95066314+Muhammadyusufsaid3@users.noreply.github.com>
2021-11-30 21:17:49 +0100

    Add files via upload

---
+++ script_muhd.py
@@ -0,0 +1,8 @@
+#<-----------Adamu Project------------>
+print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+
+print("\t\tHello Muhammad Yusuf Saidu, below are your informations:\n")
+print("Name: " + "\t\t\t" + "Muhammad Yusuf Saidu")
+print("Email: " + "\t\t\t"+ "yusufsaidmuhammad3@gmail.com")
+print("Slack_username: " + "\t" + "@Muhammad_Y_Saidu")
+print("Area of interest: " + "\t" + "Metagenomics")
