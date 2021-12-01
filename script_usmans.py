f4c0d83dad5802c4246f287b829dfd943dde678a
Usmansaidu1 <95228728+Usmansaidu1@users.noreply.github.com>
2021-12-01 14:43:32 +0100

    Add files via upload

---
+++ script_usmans.py
@@ -0,0 +1,8 @@
+#<-----------Ibrahim Project------------>
+print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+
+print("\t\tHello Usman Saidu, below are your informations:\n")
+print("Name: " + "\t\t\t" + "Usman Saidu")
+print("Email: " + "\t\t\t"+ "usmansaidu002018@gmail.com")
+print("Slack_username: " + "\t" + "@usman_saidu")
+print("Area of interest: " + "\t" + "transcriptomics")
