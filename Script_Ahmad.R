1cf371e9cf123188bea947fb2291cc77862ff21c
Teejay19 <95163599+Teejay19@users.noreply.github.com>
2021-12-01 15:45:08 +0100

    Add files via upload

---
+++ Script_Ahmad.R
@@ -0,0 +1,18 @@
+# NAME
+name<- c("Ahmad Muhammad")
+name
+
+# EMAIL ADDRESS
+email<- c("tijeyahmad@gmail.com")
+email
+
+# SLACK USERNAME
+slack<- c("@ahmadmuhammad")
+slack
+
+# BIOINFORMATIC AREA OF INTEREST
+bioinformatic<- c("genomics")
+bioinformatic
+
+
+
