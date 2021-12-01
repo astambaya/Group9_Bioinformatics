fd6a40fa8cf1dd0a8e439ef9fe003e5465e3358d
asiyatijjaniabubakar <95055174+asiyatijjaniabubakar@users.noreply.github.com>
2021-12-01 11:42:09 +0100

    Add files via upload

---
+++ Script_Asiya.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
+    cout << "\t\tHello Asiya Tijjani Abubakar:\n\n";
+    cout << "Name:"<< "\t\t\t" << "Asiya Tijjani Abubakar \n";
+    cout << "Email:"<< "\t\t\t" << "asiyatijjani150@gmail.com\n";
+    cout << "Slack_Id:" << "\t\t" << "@Asiya_Tijjani_Abubakar \n";
+    cout << "Area Of Interest:" << "\t" << "Proteomics \n";
+    return 0;
+}
