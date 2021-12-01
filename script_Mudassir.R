de1c8da5a6fcb531e0914899037b3768ed7ebef3
Mudassir Alhassan <95180487+MudassirAlhassan0806702@users.noreply.github.com>
2021-12-01 14:53:37 +0100

    Add files via upload

---
+++ script_Mudassir.R
@@ -0,0 +1,17 @@
+getwd
+
+# NAME
+name<- c('mudassir alhassan')
+name
+
+# EMAIL ADDRESS
+email<- c('mudassiralhassan070@gmail.com')
+email
+
+# SLACK USERNAME
+slack<- c('@mudassiralhassan')
+slack
+
+# BIOINFORMATIC AREA OF INTEREST
+bioinformatic<- c('genomics')
+bioinformatic
