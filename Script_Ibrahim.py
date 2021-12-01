da21bffb14ad055acf8aaedf353e89b57e50b168
Ibraheemchooner10 <95000701+Ibraheemchooner10@users.noreply.github.com>
2021-12-01 17:31:04 +0100

    Add files via upload

---
+++ Script_Ibrahim.py
@@ -0,0 +1,8 @@
+#<-----------Ibrahim Project------------>
+print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+
+print("\t\tHello Abubakar Ibrahim Ahmad, below are your informations:\n")
+print("Name: " + "\t\t\t" + "Abubakar Ibrahim Ahmada")
+print("Email: " + "\t\t\t"+ "ibraheemdanshehu@gmail.com")
+print("Slack_username: " + "\t" + "@Abubakar_I._Ahmad")
+print("Area of interest: " + "\t" + "Metagenomics")
