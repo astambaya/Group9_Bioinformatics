0a6f6d1dbe87b6e27f52521cb38e3bf8e79a1bde
jamcyadam <95356627+jamcyadam@users.noreply.github.com>
2021-12-01 14:34:35 +0100

    Add files via upload

---
+++ Script_Jamila.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
+    cout << "\t\tHello Jamila Adam Zakariyya:\n\n";
+    cout << "Name:"<< "\t\t\t" << "Jamila Adam Zakariyya\n";
+    cout << "Email:"<< "\t\t\t" << "jamcyadam01@gmail.com\n";
+    cout << "Slack_Id:" << "\t\t" << "@Jamila Adam Zakariyya \n";
+    cout << "Area Of Interest:" << "\t" << "Proteomics \n";
+    return 0;
+}
