d23f7736bfab4a42f8f38b5a6c50ca1e743ebaae
Ualiyu <95139517+Ualiyu@users.noreply.github.com>
2021-12-01 10:52:59 +0100

    Add files via upload

---
+++ Script_usman.R
@@ -0,0 +1,15 @@
+# NAME
+
+name<- c("usman aliyu")
+print(name)
+
+# EMAIL ADDRESS
+email<-c('ualiyu939@gmail.com)
+print(email')
+
+# BIOINFORMATIC COURSE OF INTEREST
+bioinformatic <-('genomics')
+print(bioinformatic)
+# SLACK USERNAME
+slack <- c("usman aliyu")
+print(slack)
