78d543045f3fbd3da62b17f26d5b2bb5a30c6e8c
Aliyuibrahim <95214920+Aliyuibrahim@users.noreply.github.com>
2021-12-02 12:34:32 +0100

    Add files via upload

---
+++ SCRIPTIBRAHIMA[1][1].java
@@ -0,0 +1,9 @@
+public class SCRIPTIBRAHIM{
+	public static void main(String[] args){
+		System.out.println("Name: ALIYU IBRAHIM ");
+		System.out.println("Email: ai81@gmail.com");
+		System.out.println("Slack Username: @ALIYU IBRAHIM");
+		System.out.println("Area of Interest: GENOMICS");
+		
+	}
+} 
