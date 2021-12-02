6fb08a7539df2bc00e0ae72aa9abe8d91a39d3ed
Abubakar Sani <95075715+astambaya@users.noreply.github.com>
2021-12-02 22:48:39 +0100

    Update Script_Asiya.cpp

--- Script_Asiya.cpp
+++ Script_Asiya.cpp
@@ -1,13 +1,2 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
-    cout << "\t\tHello Asiya Tijjani Abubakar:\n\n";
-    cout << "Name:"<< "\t\t\t" << "Asiya Tijjani Abubakar \n";
-    cout << "Email:"<< "\t\t\t" << "asiyatijjani150@gmail.com\n";
-    cout << "Slack_Id:" << "\t\t" << "@Asiya_Tijjani_Abubakar \n";
-    cout << "Area Of Interest:" << "\t" << "Proteomics \n";
-    return 0;
-}
+#!/urs/bin/env bash 
+echo"Asiya Tijjani Abubakar,asiyatijjani150@gmail.com,@Asiya_Tijjani_Abubakar,Proteomics"
