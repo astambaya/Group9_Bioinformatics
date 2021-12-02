557c6e8df8d23d560baa9b7b0488c78bde976148
Abubakar Sani <95075715+astambaya@users.noreply.github.com>
2021-12-02 22:51:09 +0100

    Update Script_Bashir.cpp

--- Script_Bashir.cpp
+++ Script_Bashir.cpp
@@ -1,13 +1,2 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
-    cout << "\t\tHello Bashir Ismail:\n\n";
-    cout << "Name:"<< "\t\t\t" << "Bashir Ismail\n";
-    cout << "Email:"<< "\t\t\t" << "bashirismail3331@gmail.com\n";
-    cout << "Slack_Id:" << "\t\t" << "@Bashir_Ismail \n";
-    cout << "Area Of Interest:" << "\t" << "Proteomics \n";
-    return 0;
-}
+#!/urs/bin/env bash
+echo"Bashir Ismail,bashirismail3331@gmail.com,@Bashir_Ismail,Proteomics"
