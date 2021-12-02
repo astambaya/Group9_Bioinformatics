07d7e9ca543754e5713f57c968b3e9879a733ee6
bashirismailjma <95217640+bashirismailjma@users.noreply.github.com>
2021-12-02 14:15:39 +0100

    Add files via upload

---
+++ Script_Bashir.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
+    cout << "\t\tHello Bashir Ismail:\n\n";
+    cout << "Name:"<< "\t\t\t" << "Bashir Ismail\n";
+    cout << "Email:"<< "\t\t\t" << "bashirismail3331@gmail.com\n";
+    cout << "Slack_Id:" << "\t\t" << "@Bashir_Ismail \n";
+    cout << "Area Of Interest:" << "\t" << "Proteomics \n";
+    return 0;
+}
